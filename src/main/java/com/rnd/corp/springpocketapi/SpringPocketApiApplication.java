82717796b7be168333cbaf16b77e9f939352b184
Mika Randrianaivo <mika.randrianaivo@sully-group.fr>
2021-03-24 17:07:32 +0100

    init api

---
+++ src/main/java/com/rnd/corp/springpocketapi/SpringPocketApiApplication.java
@@ -0,0 +1,13 @@
+package com.rnd.corp.springpocketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPocketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnd/corp/springpocketapi/SpringPocketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnd.corp.springpocketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPocketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
